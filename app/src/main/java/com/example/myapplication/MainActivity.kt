6c8752dea1406ca0a4579d21c2078b6082e6078f
Sanchit Monga <sanchitmonga22@gmail.com>
2022-09-01 14:47:31 -0400

    Added initial files
    Created customer activity and admin activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initUI()
+        setContentView(binding.root)
+    }
+
+    private fun initUI() {
+        binding.adminButton.setOnClickListener {
+            startActivity(Intent())
+        }
+
+        binding.customerButton.setOnClickListener {
+
+        }
     }
 }
